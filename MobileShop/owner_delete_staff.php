f26e38ecb84b6d75e5a58279d11c0cca93b26cca
Rojin Roy <125713908+rojinroyjo24@users.noreply.github.com>
2025-06-27 12:18:46 +0530

    Delete MobileShop directory

--- MobileShop/owner_delete_staff.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("connection.php");
-session_start();
-$oid=$_GET['id'];
-$ret="  delete FROM owner where oid='$oid'";
-echo $ret;
-mysqli_query($conn,$ret);
-header("location:view_customer.php");
-?>
